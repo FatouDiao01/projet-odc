c71d5ddf927bdd2f975d7fd7a58df1c1d9f79d18
FatouDiao01 <fatihametdiao08@gmail.com>
2025-05-17 01:47:30 +0000

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,5 +2,5 @@
 # providers.tf
 #######################################
 provider "kubernetes" {
-  config_path = "~/.kube/config"  # Adapte ce chemin à ton environnement Jenkins si besoin
+  config_path = "C:\\Users\\HP G3\\.kube\\config"
 }
