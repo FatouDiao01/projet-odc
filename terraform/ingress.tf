ea43d1b747804db25ce6bb026c5d5b8d46f6bb64
FatouDiao01 <fatihametdiao08@gmail.com>
2025-05-17 03:10:02 +0000

    Create ingress.tf

---
+++ terraform/ingress.tf
@@ -0,0 +1,46 @@
+resource "kubernetes_ingress_v1" "myapp_ingress" {
+  metadata {
+    name = "myapp-ingress"
+    annotations = {
+      "nginx.ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+
+    rule {
+      host = "myapp.local"
+
+      http {
+        path {
+          path      = "/api"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = kubernetes_service.backend.metadata[0].name
+              port {
+                number = 8080
+              }
+            }
+          }
+        }
+
+        path {
+          path      = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = kubernetes_service.frontend.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
