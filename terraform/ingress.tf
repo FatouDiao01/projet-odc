4fbde1ecc751fb9b4baafb668df7d66c9085f781
FatouDiao01 <fatihametdiao08@gmail.com>
2025-05-19 11:05:57 +0000

    Update ingress.tf

--- terraform/ingress.tf
+++ terraform/ingress.tf
@@ -9,24 +9,11 @@ resource "kubernetes_ingress_v1" "myapp_ingress" {
   spec {
     ingress_class_name = "nginx"
 
+    # Règle pour le frontend (myapp.local)
     rule {
       host = "myapp.local"
 
       http {
-        path {
-          path      = "/api"
-          path_type = "Prefix"
-
-          backend {
-            service {
-              name = kubernetes_service.backend.metadata[0].name
-              port {
-                number = 8080
-              }
-            }
-          }
-        }
-
         path {
           path      = "/"
           path_type = "Prefix"
@@ -36,6 +23,27 @@ resource "kubernetes_ingress_v1" "myapp_ingress" {
               name = kubernetes_service.frontend.metadata[0].name
               port {
                 number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+
+    # Règle pour le backend (api.local)
+    rule {
+      host = "api.local"
+
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+
+          backend {
+            service {
+              name = kubernetes_service.backend.metadata[0].name
+              port {
+                number = 8080
               }
             }
           }
